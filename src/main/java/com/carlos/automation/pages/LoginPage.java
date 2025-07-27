6b292e4778dfb3f2993beef5703e0dedc5dcae81
cvega <carlosjuniorrvega@hotmail.com>
2025-07-27 00:15:15 -0500

    feat: add LoginPage with POM structure and loginAs() helper. CV

---
+++ src/main/java/com/carlos/automation/pages/LoginPage.java
@@ -0,0 +1,34 @@
+package com.carlos.automation.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage {
+    //locators
+    private final By usernameInput = By.id("user-name");
+    private final By passwordInput = By.id("password");
+    private final By loginButton = By.id("login-button");
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void enterUsername(String username) {
+        type(usernameInput, username);
+    }
+
+    public void enterPassword(String text) {
+        type(passwordInput, text);
+    }
+
+    public void clickLogin() {
+        click(loginButton);
+    }
+
+    public void loginAs(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+    }
+}
+
