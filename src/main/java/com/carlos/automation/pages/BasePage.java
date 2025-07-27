274c191abcd9e94196811f9f829cf2ba81f05138
cvega <carlosjuniorrvega@hotmail.com>
2025-07-26 23:27:28 -0500

    feat(basepage): add reusable find, type and click interaction methods. CV

--- src/main/java/com/carlos/automation/pages/BasePage.java
+++ src/main/java/com/carlos/automation/pages/BasePage.java
@@ -1,7 +1,10 @@
 package com.carlos.automation.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
+
 
 import java.time.Duration;
 
@@ -13,4 +16,33 @@ public abstract class BasePage {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
+
+    //Methods
+
+    /**
+     * find   returns a WebElement using the provided locator
+     */
+
+    protected WebElement find(By locator) {
+        return driver.findElement(locator);
+    }
+
+    /**
+     * type   clears and types the given text into the located input field
+     */
+    protected void type(By locator, String text) {
+        find(locator).clear();
+        find(locator).sendKeys(text);
+    }
+
+    /**
+     * click  clicks the element identified by the locator
+     */
+    protected void click(By locator) {
+        find(locator).click();
+    }
 }
+
+
+
+
